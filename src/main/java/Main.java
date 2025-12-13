5828c1b2e8a46bccf5365e6d5e3f2946c125abd6
ELMAALMIA <ayoubelmaalmi@gmail.com>
2025-12-13 16:46:28 +0100

    feat : add REAPL loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
+
         boolean validate = false;
         Scanner scanner = new Scanner(System.in);
-        String  cmd = scanner.nextLine();
-        if (!validate){
-            System.out.print(cmd+": command not found");
+        while (true){
+            System.out.print("$ ");
+            String  cmd = scanner.nextLine();
+            if (!validate){
+                System.out.println(cmd+": command not found");
+            }
         }
+
     }
 }
