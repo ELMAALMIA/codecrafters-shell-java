7466b8fe4b91bc75e5f1a266b69dbb82ebb918fa
ELMAALMIA <ayoubelmaalmi@gmail.com>
2025-12-13 22:31:55 +0100

    feat : add builtin via path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,52 @@
+import java.io.File;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-
-        boolean validate = false;
         Scanner scanner = new Scanner(System.in);
+        String pathEnv = System.getenv("PATH");
+        String[] directories = {};
+        if (pathEnv != null) {
+            directories = pathEnv.split(":");
+        }
+        Set<String> builtins = Set.of("exit", "echo", "type");
 
-        while (true){
+        while (true) {
             System.out.print("$ ");
-            String  cmd = scanner.nextLine();
-            String cmdtab[] =cmd.split(" ");
-            if (cmdtab[0].equals("type")){
-                validate = true;
-                if(cmdtab[1].equals("echo")){
-                    System.out.println("echo is a shell builtin");
-                }
-                else if(cmdtab[1].equals("exit")) System.out.println("exit is a shell builtin");
-                else if(cmdtab[1].equals("type")) System.out.println("type is a shell builtin");
-                else System.out.println(cmdtab[1]+": not found");
+            String cmd = scanner.nextLine();
+            String[] cmdTab = cmd.split(" ");
 
-            }
-
-
-            if(cmdtab[0].equals("echo")){
-                validate = true;
-                for (int i = 1; i < cmdtab.length; i++) {
-                    System.out.print(cmdtab[i] + " ");
+            if (cmdTab[0].equals("exit")) {
+                break;
+            } else if (cmdTab[0].equals("echo")) {
+                for (int i = 1; i < cmdTab.length; i++) {
+                    if (i > 1) System.out.print(" ");
+                    System.out.print(cmdTab[i]);
                 }
                 System.out.println();
+            } else if (cmdTab[0].equals("type")) {
+                if (builtins.contains(cmdTab[1])) {
+                    System.out.println(cmdTab[1] + " is a shell builtin");
+                } else {
+                    boolean found = false;
+                    if (pathEnv != null) {
+                        for (String dir : directories) {
+                            File file = new File(dir, cmdTab[1]);
+                            if (file.exists() && file.canExecute()) {
+                                System.out.println(cmdTab[1] + " is " + file.getAbsolutePath());
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!found) {
+                        System.out.println(cmdTab[1] + ": not found");
+                    }
+                }
+            } else {
+                System.out.println(cmdTab[0] + ": command not found");
             }
-            if(cmd.equals("exit")){
-                break;
-            }
-            if (!validate){
-                System.out.println(cmd+": command not found");
-            }
-            validate=false;
-
-
-
-
-
         }
-
     }
 }
